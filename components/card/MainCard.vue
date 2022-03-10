f8b648434207fc87f535fda28c42da41ac5a67ff
yeonwoochoi <chldusdn20@gmail.com>
2022-03-10 20:57:52 +0900

    refactor: fix margin during mobile breakpoint

--- components/card/MainCard.vue
+++ components/card/MainCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="elevation-0 card-flat mb-6 py-8 pl-8">
+  <v-card class="elevation-0 card-flat mb-6 py-8 pl-md-8">
     <v-card-title>
       <p class="font-weight-light my-4" style="font-size: 40px; letter-spacing: -0.5px;">
         {{ header }}
