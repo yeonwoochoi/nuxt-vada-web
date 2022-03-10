2165885f6220115c9f45162ac86f1e5a85c9a912
yeonwoochoi <chldusdn20@gmail.com>
2022-03-10 21:00:41 +0900

    feat: add purchase list card in my-page

--- components/card/my-page/PurchaseListCard.vue
+++ components/card/my-page/PurchaseListCard.vue
@@ -1,12 +1,31 @@
 <template>
-  <v-container fluid>
+  <v-scroll-y-transition mode="out-in">
+    <v-card
+      class="mx-auto"
+      flat
+      style="width: 100%; height: fit-content;"
+    >
+      <v-card-title class="pb-0">
+        <p class="font-weight-medium display-1">
+          {{ header }}
+        </p>
+      </v-card-title>
+      <v-divider style="background-color:#555555; border-width: 1px !important;"/>
 
-  </v-container>
+    </v-card>
+  </v-scroll-y-transition>
 </template>
 
 <script>
 export default {
-  name: "PurchaseListCard"
+  name: "PurchaseListCard",
+  props: {
+    header: {
+      type: String,
+      default: () => ''
+    },
+
+  }
 }
 </script>
 
