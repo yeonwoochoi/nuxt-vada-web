cf7bd7ffcd169001314d562efde2d524816ec403
yeonwoochoi <chldusdn20@gmail.com>
2022-03-15 09:35:47 +0900

    feat: separate data table showing only data values into components

---
+++ components/table/SimpleDataTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-data-table
+    :headers="tableHeader"
+    :items="tableContent"
+    hide-default-footer
+    class="text-center"
+    :mobile-breakpoint="mobileBreakpoint"
+  />
+</template>
+
+<script>
+export default {
+  name: "SimpleDataTable",
+  props: {
+    tableHeader: {
+      type: Array,
+      default: () => []
+    },
+    tableContent: {
+      type: Array,
+      default: () => []
+    },
+    mobileBreakpoint: {
+      type: Number,
+      default: () => 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
