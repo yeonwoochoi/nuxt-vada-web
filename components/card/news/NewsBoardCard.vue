3b952bee46581dddea2f1180b364ecf937be2f86
yeonwoochoi <chldusdn20@gmail.com>
2022-03-16 14:01:42 +0900

    refactor: excludes sorting, search item selection functions

--- components/card/news/NewsBoardCard.vue
+++ components/card/news/NewsBoardCard.vue
@@ -1,33 +1,7 @@
 <template>
   <v-row align="center" justify="center" class="mb-6 pl-6" style="width: 100%;">
-    <v-col cols="6" align="start" class="py-0 pl-4">
+    <v-col cols="12" align="start" class="py-0 pl-4">
       <p class="subtitle-2 my-0">전체 {{ contentCount }}</p>
-    </v-col>
-    <v-col cols="6" align="end" class="py-0">
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            small
-            outlined
-            v-bind="attrs"
-            v-on="on"
-            class="elevation-0"
-            style="max-width: 110px; max-height: 30px; border: rgb(200, 200, 200) 0.01em solid; background-color: transparent"
-          >
-            {{ currentSortBy }}
-            <v-icon>mdi-menu-down</v-icon>
-          </v-btn>
-        </template>
-        <v-list dense>
-          <v-list-item
-            v-for="(item, index) in sortableItems"
-            :key="index"
-            @click="changeSortBy(item)"
-          >
-            <v-list-item-title class="caption">{{ item }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-col>
     <v-col cols="12">
       <v-data-table
@@ -58,7 +32,7 @@
         </template>
         <template v-slot:footer>
           <v-pagination
-            v-model="currentPage"
+            v-model="page"
             :length="totalPage"
             total-visible="10"
             prev-icon="mdi-menu-left"
@@ -87,7 +61,7 @@
         </template>
         <template v-slot:footer>
           <v-pagination
-            v-model="currentPage"
+            v-model="page"
             :length="totalPage"
             total-visible="10"
             prev-icon="mdi-menu-left"
@@ -100,29 +74,6 @@
     </v-col>
     <v-col cols="12" sm="10" md="8" xl="6" class="mt-4 mb-12">
       <div style="display: flex; justify-content: center; height: 30px;">
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              small
-              v-bind="attrs"
-              v-on="on"
-              class="elevation-0 mr-4 mt-1 text-start"
-              style="min-width: 80px; height: 100%; border: rgb(200, 200, 200) 0.05em solid; background-color: transparent"
-            >
-              {{ searchBy }}
-              <v-icon>mdi-menu-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list dense>
-            <v-list-item
-              v-for="(item, index) in searchableItems"
-              :key="index"
-              @click="changeSearchBy(item)"
-            >
-              <v-list-item-title class="caption">{{ item }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
         <v-text-field
           v-model="searchInputText"
           label="Search"
@@ -160,21 +111,29 @@ export default {
     isLoading: {
       type: Boolean,
       default: () => false
-    }
+    },
+    itemsPerPage: {
+      type: Number,
+      default: () => 10
+    },
+    currentPage: {
+      type: String,
+      default: () => '1'
+    },
+    keyword: {
+      type: String,
+      default: () => ''
+    },
   },
-  created() {
+  mounted() {
     this.init();
   },
   data: () => ({
-    // query에 들어있는 값, server에 request 변수에 최종적으로 넣어줄 값
     search: '',
-    // v-model로 검색 input field에 binding될 값
-    // 합치면 오류뜨길래 일부러 분리함.
     searchInputText: '',
-    sortBy: '',
-    searchBy: '',
-    currentPage: 1,
-    itemsPerPage: 10,
+    sortBy: 'no',
+    searchBy: '전체',
+    page: 1,
     sortDesc: true,
     headers: [
       {
@@ -223,46 +182,11 @@ export default {
         value: 'view_count',
       },
     ],
-    // 말 그대로 v-menu에 들어갈 button text array
-    sortableItems: [],
-    // sortableItems 중에 하나를 누르면 이 Ref를 참조해 header 값을 찾아내고 이를 통해 value들을 분류함
-    sortableItemsRef: [
-      {
-        view: '최신순',
-        value: 'no'
-      },
-      {
-        view: '업데이트순',
-        value: 'created_at'
-      },
-      {
-        view: '조회순',
-        value: 'view_count'
-      },
-    ],
-    searchableItems: [],
-    searchableItemsRef: [
-      {
-        view: '제목',
-        value: 'title'
-      },
-      {
-        view: '내용',
-        value: 'content'
-      },
-      {
-        view: '작성자',
-        value: 'author'
-      },
-    ],
     noDataText: 'There is no data',
   }),
   computed: {
     currentPath() {
       return this.$router.currentRoute.path;
-    },
-    currentSortBy() {
-      return this.sortableItemsRef.find(v => v.value === this.sortBy).view;
     },
     isMobile () {
       switch (this.$vuetify.breakpoint.name) {
@@ -276,78 +200,19 @@ export default {
     }
   },
   methods: {
-    // 초기 설정
     init() {
-      // v-menu 에서 sortBy 되는 Header만 추려서 v-menu list에 등록하는 과정
-      // 최신순 (no) 업데이트순 (created_at) 조회순 (view_count)
-      let result = [];
-      if (this.headers) {
-        for (let i = 0; i < this.headers.length; i++) {
-          let sortableItem = this.sortableItemsRef.find(v => v.value === this.headers[i].value);
-          if (sortableItem) {
-            result.push(sortableItem.view);
-          }
-        }
-      }
-      this.sortableItems = result;
-      // 검색 범위 설정할 때, 검색 가능한 Header만 추려서 설정
-      result = [];
-      result.push('전체')
-      for (let i = 0; i < this.searchableItemsRef.length; i++) {
-        result.push(this.searchableItemsRef[i].view)
-      }
-      this.searchableItems = result;
-      this.searchBy = this.searchableItems[0];
-      // query에 있는 검색 조건, page 등을 받아옴
-      this.currentPage = parseInt(this.$route.query.page)
-      this.sortBy = this.$route.query.board_list_sort
-      this.itemsPerPage = parseInt(this.$route.query.items_per_page)
-      if (this.$route.query.keyword) {
-        this.search = this.$route.query.keyword
-        let searchBy = this.$route.query.target;
-        if (searchBy === 'total') {
-          this.searchBy = '전체'
-        } else {
-          this.searchBy = this.searchableItemsRef.find(v => v.value === searchBy).view;
-        }
-        this.searchInputText = this.search
-      } else {
-        this.search = '';
-        this.searchInputText = this.search;
-        this.searchBy = '전체';
-      }
+      this.page = parseInt(this.currentPage)
+      this.search = !this.keyword ? '' : this.keyword;
+      this.searchInputText = this.search
     },
     // query 값 중 page 값이 바뀌면 호출됨
     changePage() {
-      let searchBy = this.searchBy === '전체' ? 'total' : this.searchableItemsRef.find(v => v.view === this.searchBy).value
+      let query = !this.search
+        ? { page: this.page }
+        : { page: this.page, keyword: this.search};
       this.$router.push({
         path: this.currentPath,
-        query: {
-          page: this.currentPage,
-          board_list_sort: this.sortBy,
-          items_per_page: this.itemsPerPage,
-          keyword: this.search,
-          target: searchBy
-        }
-      })
-    },
-    // query 값 중 sortBy 값이 바뀌면 호출됨
-    changeSortBy(sortBy) {
-      let targetVal = this.sortableItemsRef.find(v => v.view === sortBy).value
-      let target = this.headers.find(h => h.value === targetVal);
-      if (!target) {
-        this.sortBy = 'no'
-      }
-      else {
-        this.sortBy = targetVal
-      }
-      this.$router.push({
-        path: this.currentPath,
-        query: {
-          page: 1,
-          board_list_sort: this.sortBy,
-          items_per_page: this.itemsPerPage
-        }
+        query: query
       })
     },
     // searchBy v-menu 누르면 호출됨
@@ -359,16 +224,12 @@ export default {
     // 검색 버튼 눌렀을 때
     onClickSearchBtn () {
       this.search = this.searchInputText
-      let searchBy = this.searchBy === '전체' ? 'total' : this.searchableItemsRef.find(v => v.view === this.searchBy).value
-      this.currentPage = 1;
+      this.page = 1;
       this.$router.push({
         path: this.currentPath,
         query: {
-          page: this.currentPage,
-          board_list_sort: this.sortBy,
-          items_per_page: this.itemsPerPage,
+          page: this.page,
           keyword: this.search,
-          target: searchBy
         }
       })
     },
