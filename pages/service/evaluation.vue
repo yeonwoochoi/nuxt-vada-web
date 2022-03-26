6b67ec947ffbddc9a21b95a2205c231f13d015b2
yeonwoochoi <chldusdn20@gmail.com>
2022-03-27 01:06:55 +0900

    feat: add evaluate card template

--- pages/service/evaluation.vue
+++ pages/service/evaluation.vue
@@ -1,10 +1,27 @@
 <template>
-  <p>evaluation</p>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-card style="width: 1200px; height:fit-content;" class="elevation-0">
+        <main-card :header="header">
+          <template v-slot:body>
+
+          </template>
+        </main-card>
+      </v-card>
+    </v-row>
+  </v-container>
+
 </template>
 
 <script>
+import MainCard from "../../components/card/MainCard";
 export default {
-  name: "evaluation"
+  name: "evaluation",
+  components: {MainCard},
+  data: () => ({
+    header: '특허평가',
+
+  })
 }
 </script>
 
