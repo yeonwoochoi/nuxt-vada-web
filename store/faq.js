d857e9c18e13a2a892f89f41ae85dc8df17f62d2
beagle <chldusdn20@gmail.com>
2022-04-17 13:18:53 +0900

    feat: add faq vuex

---
+++ store/faq.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+
+})
+
+export const actions = {
+  async readAll ({commit}) {
+    return new Promise(((resolve, reject) => {
+      this.$axios.$get('/faq').then(res => {
+        resolve(res['faqs'])
+      }).catch(err => {
+        reject(err.message)
+      })
+    }))
+  }
+}
