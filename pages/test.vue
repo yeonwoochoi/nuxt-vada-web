72193b74151b4e169fb1a4e01a9aa6b2ebce341b
beagle <chldusdn20@gmail.com>
2022-04-20 10:56:12 +0900

    refactor: test sample file download by pdf

--- pages/test.vue
+++ pages/test.vue
@@ -1,12 +1,40 @@
 <template>
-  <report/>
+  <div>
+    <report/>
+    <v-btn
+      @click="downloadFile"
+    >
+      다운로드
+    </v-btn>
+  </div>
 </template>
 
 <script>
 import Report from "../components/report/Report";
 export default {
   name: "test",
-  components: {Report}
+  components: {Report},
+  auth: false,
+  methods: {
+    downloadFile() {
+      this.$store.dispatch('patent/download', 'Report').then(
+        res => {
+          const url = window.URL.createObjectURL(new Blob([res.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'report-sample.docx');
+          document.body.appendChild(link);
+          link.click();
+        },
+        err => {
+          this.$notifier.showMessage({
+            content: err,
+            color: 'error'
+          })
+        }
+      )
+    }
+  }
 }
 </script>
 
