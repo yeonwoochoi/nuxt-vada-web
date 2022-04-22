752d1504b355c0421bcbfbe7944d52e149decb19
beagle <chldusdn20@gmail.com>
2022-04-22 20:42:50 +0900

    refactor: change error page design

---
+++ layouts/errorLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-container class="full-size" fluid>
+      <v-row align="center" justify="center" class="full-size">
+        <Nuxt/>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "errorLayout",
+}
+</script>
+
+<style scoped>
+.full-size {
+  width: 100%;
+  height: 100%;
+}
+</style>
