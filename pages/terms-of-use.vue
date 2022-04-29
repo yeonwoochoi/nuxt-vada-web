cf1cbd6f05020b9db113a9d6f52be8681da9b843
beagle <chldusdn20@gmail.com>
2022-04-30 01:47:29 +0900

    feat: add terms of use template

---
+++ pages/terms-of-use.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-card style="width: 1200px; height:fit-content;" class="elevation-0">
+        <main-card :header="header">
+          <template v-slot:body>
+
+          </template>
+        </main-card>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MainCard from "../components/card/MainCard";
+export default {
+  name: "terms-of-use",
+  components: {MainCard},
+  data: () => ({
+    header: '이용약관'
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
