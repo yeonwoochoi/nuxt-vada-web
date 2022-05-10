d07310f85d924b8aebb38c9920a3e09bae5221db
beagle <chldusdn20@gmail.com>
2022-05-10 14:46:29 +0900

    refactor: remove axios console

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,10 +3,5 @@ export default ({ $axios, app, error: nuxtError }, inject) => {
     if (config.url === '/user/refresh-token') {
       config.headers['Authorization'] = 'Bearer ' + app.$auth.strategy.refreshToken.get()
     }
-    console.log('Making request to ' + config.url)
-  })
-
-  $axios.onError(err => {
-    console.error(err)
   })
 }
